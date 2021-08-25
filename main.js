8dc36730d0b55614939ca8044f8570a7a2d8f8ce
RehanBeam <89525511+RehanBeam@users.noreply.github.com>
2021-08-25 08:43:52 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function alert55() {
+    window.alert("This is how alert shows up.");
+}
+function alertdesc() {
+    window.alert("This is a picture");
+}
+function playaudio() {
+    document.getElementById("audio_1").play();
+}
